7b245e736dfaebd58a08d65bc57eedee27364ad6
Marco Finelli <marcofinelli@MacBook-Pro-di-Marco.local>
2026-04-15 10:22:50 +0200

    clean config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
 import react from '@astrojs/react';
 
 export default defineConfig({
   output: 'static',
-  adapter: vercel(),
   integrations: [react()],
 });
