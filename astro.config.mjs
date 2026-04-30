675c822538393da1cb7c0dc83cb3f4a5d6703f91
Marco Finelli <marcofinelli@hotmail.it>
2026-04-30 14:54:23 +0200

    Configure site URL for SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+  site: 'https://www.retro-gamers.it',
   output: 'server',
   adapter: vercel()
 });
