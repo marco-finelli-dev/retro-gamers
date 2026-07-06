10d69c732075378c5b6dcc549a4e2e7999f6defb
Marco Finelli <marcofinelli@hotmail.it>
2026-07-06 18:14:12 +0200

    Add safe redirects for legacy URLs

--- src/middleware.ts
+++ src/middleware.ts
@@ -14,6 +14,56 @@ const legacyPlatformRedirects: Record<string, string> = {
   '/commodore-64/': '/piattaforme/computer/commodore/commodore-64/',
 };
 
+const legacyArticleRedirects: Record<string, string> = {
+  '/home': '/',
+  '/home/': '/',
+  '/2025/10/27/hello-world': '/',
+  '/2025/10/27/hello-world/': '/',
+
+  '/interviste/dynabyte-la-storia-raccontantata-da-massimo-magnasciutti':
+    '/interviste/dynabyte-nippon-safes-intervista-massimo-magnasciutti/',
+  '/interviste/dynabyte-la-storia-raccontantata-da-massimo-magnasciutti/':
+    '/interviste/dynabyte-nippon-safes-intervista-massimo-magnasciutti/',
+
+  '/intervista/luigi-marrone-di-ludenz/':
+    '/interviste/luigi-marrone-ludenz-intervista-rabdomantica-game-culture/',
+  '/intervista/luigi-marrone-di-ludenz':
+    '/interviste/luigi-marrone-ludenz-intervista-rabdomantica-game-culture/',
+  '/interviste/luigi-marrone-di-ludenz/':
+    '/interviste/luigi-marrone-ludenz-intervista-rabdomantica-game-culture/',
+  '/interviste/luigi-marrone-di-ludenz':
+    '/interviste/luigi-marrone-ludenz-intervista-rabdomantica-game-culture/',
+
+  '/recensioni/pang-commodore-amiga/': '/recensioni/pang/',
+  '/recensioni/pang-commodore-amiga': '/recensioni/pang/',
+
+  '/speciale/speciale-kiss-videogiochi-psycho-circus-tony-hawk-cameo/':
+    '/speciali/speciale-kiss-videogiochi-psycho-circus-tony-hawk/',
+  '/speciale/speciale-kiss-videogiochi-psycho-circus-tony-hawk-cameo':
+    '/speciali/speciale-kiss-videogiochi-psycho-circus-tony-hawk/',
+
+  '/guida/installare-cf-su-amiga-come-hard-disk/':
+    '/guide/compact-flash-amiga-guida-completa-hard-disk-a500-a1200-whdload/',
+  '/guida/installare-cf-su-amiga-come-hard-disk':
+    '/guide/compact-flash-amiga-guida-completa-hard-disk-a500-a1200-whdload/',
+  '/guida/compact-flash-amiga-come-hard-disk/':
+    '/guide/compact-flash-amiga-guida-completa-hard-disk-a500-a1200-whdload/',
+  '/guida/compact-flash-amiga-come-hard-disk':
+    '/guide/compact-flash-amiga-guida-completa-hard-disk-a500-a1200-whdload/',
+  '/guida/compact-flash-amiga-come-hd/':
+    '/guide/compact-flash-amiga-guida-completa-hard-disk-a500-a1200-whdload/',
+  '/guida/compact-flash-amiga-come-hd':
+    '/guide/compact-flash-amiga-guida-completa-hard-disk-a500-a1200-whdload/',
+  '/guide/installare-cf-su-amiga-come-hard-disk/':
+    '/guide/compact-flash-amiga-guida-completa-hard-disk-a500-a1200-whdload/',
+  '/guide/installare-cf-su-amiga-come-hard-disk':
+    '/guide/compact-flash-amiga-guida-completa-hard-disk-a500-a1200-whdload/',
+  '/guide/usare-il-gotek-drive-esternamente-come-df0/':
+    '/guide/gotek-amiga-esterno-df0-guida/',
+  '/guide/usare-il-gotek-drive-esternamente-come-df0':
+    '/guide/gotek-amiga-esterno-df0-guida/',
+};
+
 function permanentRedirect(url: URL, destination: string) {
   const target = new URL(destination, url.origin);
 
@@ -28,6 +78,10 @@ function permanentRedirect(url: URL, destination: string) {
 export const onRequest = defineMiddleware(async (context, next) => {
   const { url } = context;
   const pathname = url.pathname;
+
+  if (legacyArticleRedirects[pathname]) {
+    return permanentRedirect(url, legacyArticleRedirects[pathname]);
+  }
 
   if (legacyPlatformRedirects[pathname]) {
     return permanentRedirect(url, legacyPlatformRedirects[pathname]);
