12751b0dbc71e7095ee30cbd38d1f7720432a495
Marco Finelli <marcofinelli@hotmail.it>
2026-07-15 22:20:03 +0200

    Fix dynamic platform visibility

--- src/lib/sanity.js
+++ src/lib/sanity.js
@@ -3,13 +3,15 @@ import { createClient } from '@sanity/client';
 const projectId = 'y88ky0mu';
 const dataset = 'production';
 const apiVersion = '2024-01-01';
+const readToken = process.env.SANITY_API_READ_TOKEN;
 
 export const client = createClient({
   projectId,
   dataset,
   apiVersion,
   useCdn: false,
-  perspective: 'published'
+  perspective: 'published',
+  token: readToken
 });
 
 export const previewClient = createClient({
